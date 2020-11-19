097c4ac8ddc99efcf9f6ad14be6cdb16242c46ca
Aditi Ohri <didiohri@gmail.com>
2020-11-18 21:25:30 -0600

    add 404 Not Found page for unknown routes

--- src/App.js
+++ src/App.js
@@ -18,8 +18,10 @@ class App extends Component {
             <NavLink to="/courses">Courses</NavLink>
           </nav>
           <Switch>
+            <Route path="/" exact />
             <Route path="/users" component={Users} />
             <Route path="/courses" component={Courses} />
+            <Route path="/" render={() => <h1>404 Not Found</h1>} />
           </Switch>
           <ol className="tasks">
             <li className="done">
@@ -35,16 +37,18 @@ class App extends Component {
               the "Course" component in the place of "Courses" (without passing
               any data for now)
             </li>
-            <li className="done">Pass the course ID to the "Course" page and output it there</li>
+            <li className="done">
+              Pass the course ID to the "Course" page and output it there
+            </li>
             <li className="done">
               Pass the course title to the "Course" page - pass it as a param or
               score bonus points by passing it as query params (you need to
               manually parse them though!)
             </li>
-            <li>
+            <li className="done">
               Load the "Course" component as a nested component of "Courses"
             </li>
-            <li>Add a 404 error page and render it for any unknown routes</li>
+            <li className="done">Add a 404 error page and render it for any unknown routes</li>
             <li>
               Redirect requests to /all-courses to /courses (=> Your "Courses"
               page)
